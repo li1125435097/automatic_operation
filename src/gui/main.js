9320c2ca215f6c2b24900f580fa7e1a7d3a0a51a
“lijinke” <lijinke@xiaoyun.com>
2023-01-09 19:59:11 +0800

    init

---
+++ src/gui/main.js
@@ -0,0 +1,9 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+const createWindow = () => {
+  const win = new BrowserWindow({width:800,height:600})
+  win.loadFile(path.resolve(__dirname,'index.html'))
+}
+
+app.whenReady().then(()=>{createWindow()})
